49a78775d44dddb38aa22afcad886d2a0a34e282
Ayush Trivedi <ayushtrivedi2955@gmail.com>
2023-06-15 04:53:45 +0530

    Production v1.0

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,2 +1,2 @@
-export const backendUrl = "http://localhost:8000";
-export const clientUrl ="http://localhost:5173";
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const clientUrl =import.meta.env.VITE_FRONTEND_URL;
